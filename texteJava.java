a4362e68692251ca86baa20cef9e18ed26a77529
Marcelo Manzo <marcelohmanzo@gmail.com>
2026-06-01 21:11:47 -0300

    Correção do arquivo em java

--- texteJava.java
+++ texteJava.java
@@ -1,14 +1,18 @@
 public class texteJava {
     int raio;
 
-    // Correção: Adicionado o tipo 'int' para o parâmetro
     public void setRaio(int raio) {
         this.raio = raio;
     }
 
-    // Correção: Mudou de 'int' para 'void', pois o método apenas imprime e não retorna valor
     public void retornarPerimetro() {
         double perimetro = this.raio * 2 * 3.14;
         System.out.println(perimetro);
+    }
+
+    public static void main(String[] args) {
+        texteJava teste = new texteJava();
+        teste.setRaio(2);
+        teste.retornarPerimetro();
     }
 }
